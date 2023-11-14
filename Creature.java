1de5dc7bae521e8728f18739aed696221794beb3
ValeriaPoveda <145398533+ValeriaPoveda@users.noreply.github.com>
2023-11-13 20:37:13 -0700

    Create Creature.java

---
+++ Creature.java
@@ -0,0 +1,18 @@
+public class Creature{
+  private String name;
+  private double replicationChange;
+  private double deathChange;
+
+public Creature(String name, double replicationChange, double deathChance)
+  this.name = name;
+  this.replicationChange = replicationChange;
+  this.deathChange = deathChange;
+}
+
+public void die(){
+}
+
+public Creature reproduce (){
+}
+
+
