e14dd8f1358c8c465f795915b3b274279ab66da0
ValeriaPoveda <145398533+ValeriaPoveda@users.noreply.github.com>
2023-11-13 23:40:59 -0700

    Update World.java

--- World.java
+++ World.java
@@ -1,17 +1,24 @@
-public class World {
-  private List<Creature> creatures;
+package worldofwonders;
+
+import java.util.Random;
+
+public class World extends Environment {
   private double creatureSpawnChance;
 
   public World (double creatureSpawnChance){
-    this.creatures = new ArrayList<>();
+    super();
     this.creatureSpawnChance = creatureSpawnChance;
   }
+
+  @Override
    public void createCreature(){
      if (Math.random() < creatureSpawnChance) {
        Creature newCreature = new Creature(Name.Generator.getRandomeName(), 0.5, 0.2);
        creatures.add(newCreature);
      }
    }
+
+  @Override
   public void spawnFood(){
   }
 }
