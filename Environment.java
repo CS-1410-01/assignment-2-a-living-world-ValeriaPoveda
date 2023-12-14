2b3a532a217eb7ac8e33c76922bd3731a2f60d4f
ValeriaPoveda <145398533+ValeriaPoveda@users.noreply.github.com>
2023-12-13 20:42:50 -0700

    Update Environment.java

--- Environment.java
+++ Environment.java
@@ -2,19 +2,22 @@ package worldofwonders;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public abstract class Environment {
   protected List<Creature> creatures;
+  protected int[][] foodGrid;
 
 public Environment(){
   this.creatures = new ArrayList<>();
+  this.foodGrid = new int[150][150];
+  initializeFoodGrid();
 }
 
-public abstract void createCreature();
+public abstract void createCreature(int xPosition, int yPosition);
 public abstract void spawnFood();
 public abstract void update();
 public abstract void outputStats();
-
 public List<Creature> getCreatures(){
   return creatures;
 }
@@ -23,6 +26,14 @@ protected void updateCreatures() {
 for (Creature creature : creatures) {
   creature.updateHunger();
   creature.die();
+  Creature offspring = creature.reproduce();
+  if (offspring != null){
+    creatures.add(offspring);
+  }
+  creature.interactWithEnv(this);
+  for (Creature otherCreature : creatures){
+    if (creature != otherCreature){
+      creature.interactWithCreature(otherCreature)
 }
 }
 }
