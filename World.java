7cacc87f7149545e0599dfcabb508246a98700c9
ValeriaPoveda <145398533+ValeriaPoveda@users.noreply.github.com>
2023-12-13 21:16:44 -0700

    Update World.java

--- World.java
+++ World.java
@@ -1,39 +1,45 @@
 package worldofwonders;
 
-public class World extends Environment {
-  private int foodAvailability;
+import java.itl.Random;
 
-  public World (double initialFood) {
+public class World extends Environment {
+  public World() {
     super();
-    this.foodAvailability = (int) (initialFood * 100);
   }
 
   @Override 
-  public void createCreature() {
-    creatures.add(new Creature (NameGenerator.getRandomName(), 0.1, 0.1));
+  public void createCreature(int xPosition, int yPosition) {
+    creatures.add(new Creature (NameGenerator.getRandomName(), 0.1, 0.1, xPosition, yPosition));
   }
 
   @Override
   public void spawnFood() {
-    this.foodAvailability += 10;
+    Random random = new Random();
+    int x = random.nextInt(foodGrid.length);
+    int y = random.nextInt(foodGrid[0].length);
+    foodGrid[x][y] += 10;
   }
 
-  @Override public void update() {
+  @Override 
+  public void update() {
     for (Creature creature : creatures) {
-      creature.updateHunger();
-      creature.die();
-      Creature offspring = creature.reproduce();
-      if (offspring != null) {
-        creatures.add(offspring);
-      }
+     int newX = creature.xPosition + getRandomMovement();
+     int newY = creature.yPosition + getRandomMovement();
+     newX = Math.max(0, Math.min(newX, foodGrid.length - 1));
+     newY = Math.max(0, Math.min(newY, foodGrid[0].length - 1));
+      creature.move(newX, newY);
     }
-    this.outputStats();
+    updateCreatures();
   }
 
+  private int getRandomMovement(){
+    Random random = new Random();
+    return random.nextInt(3) -1;
+  }
+ 
   @Override
   public void outputStats(){
     System.out.println("Population: " + creatures.size());
-    System.out.println("Food Availability: " + foodAvailability);
   }
 }
   
