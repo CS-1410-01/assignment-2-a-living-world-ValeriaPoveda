09fcca2c48d713089ace0038660cc136704b5a4c
ValeriaPoveda <145398533+ValeriaPoveda@users.noreply.github.com>
2023-12-13 21:18:53 -0700

    Update LivingBeing.java

--- LivingBeing.java
+++ LivingBeing.java
@@ -3,4 +3,6 @@ package worldofwonders;
 public interface LivingBeing {
   void die();
   Creature reproduce();
+  void interactWithEnvironment(Environment environment);
+  void interactWithCreatures(Creature otherCreature);
 }
