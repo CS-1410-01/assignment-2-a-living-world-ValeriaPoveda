fd5cbfb171ad49a9b8039d762e2562eb13fc7b64
ValeriaPoveda <145398533+ValeriaPoveda@users.noreply.github.com>
2023-12-13 23:44:58 -0700

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,7 @@
 package worldofwonders;
 
+import java.util.Random;
+
 public class Hello {
   public static void main(String[] args) {
     Environment world = new World(0.5);
@@ -10,5 +12,8 @@ public class Hello {
       world.update();
       world.outputStats();
     }
+  }
+  private static int getRandomPosition(){
+    return (int) (Math.random()*150);
   }
 }
